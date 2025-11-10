1e55824d4dff33bcba68d917b45a51686f554e8d
csesarah <63842047+csesarah@users.noreply.github.com>
2025-11-10 23:25:49 +0800

    Fix: eval

--- selfplay.py
+++ selfplay.py
@@ -483,7 +483,7 @@ def create_selfplay_pool(
     pool.scan_checkpoint_dir(model_type=model_type)
     
     pool_state_path = os.path.join(checkpoint_dir, f"{model_type}_population.json")
-    pool.load_pool_state(pool_state_path)
+    # pool.load_pool_state(pool_state_path)
     
     return pool
 
